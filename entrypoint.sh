0d140c02df9302453dce307b46cf94c7f496ab4a
CKO <chris@casualcode.org>
2020-12-21 04:44:35 +0100

    ....

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,15 @@ git config --global user.email "you@example.com"
 
 git clone "https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.git" /repo
 
+cd /
+
 mkdir /wiki
-cd /wiki
-git init
-git pull https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.wiki.git ./
+
 yes | cp -rf /repo/wiki/* /wiki/
 python3 /create_sidebar.py
+
+cd /wiki
+git init
 git add .
 git commit -m "Automatic Wiki Deployment"
 git push --set-upstream https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.wiki.git master
