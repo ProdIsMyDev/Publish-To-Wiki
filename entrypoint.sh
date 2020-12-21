3a0d9c5a24ffb9bf0b51baedcb4b45ae2363773e
CKO <chris@casualcode.org>
2020-12-21 03:53:24 +0100

    hopefully fixed it

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,15 @@
 
 echo "Checking out project"
 
-git config user.name "$GITHUB_ACTOR"
-git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+git config --global user.name "$GITHUB_ACTOR"
+git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com"
 
 git clone "https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.git" repo
 git clone "https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.wiki.git" wiki
 
 
-cp repo/wiki/* wiki/
-python /create_sidebar.py
+cp -r repo/wiki/* wiki/
+python3 /create_sidebar.py
 cd wiki/
 git add .
 git commit -m "Automatic Wiki Deployment"
