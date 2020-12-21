36eed372dac24e12efa7d89286e9510113d57f6e
CKO <chris@casualcode.org>
2020-12-21 04:47:27 +0100

    ....

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ echo "Checking out project"
 
 git config --global user.name "$GITHUB_ACTOR"
 git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com"
-git config --global user.email "you@example.com"
 
 git clone "https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.git" /repo
 
@@ -17,6 +16,8 @@ python3 /create_sidebar.py
 
 cd /wiki
 git init
+git config user.name "$GITHUB_ACTOR"
+git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
 git add .
 git commit -m "Automatic Wiki Deployment"
 git push --set-upstream https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.wiki.git master
