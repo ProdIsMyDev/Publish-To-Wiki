785318ab29c5a18702405b656c643c5ba9adeceb
CKO <chris@casualcode.org>
2020-12-21 04:25:17 +0100

    PlEaSe WoRk

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,9 @@ git config --global user.email "you@example.com"
 git clone "https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.git" /repo
 git clone "https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.wiki.git" /wiki
 
+rm -rf /wiki/*
 cp -r /repo/wiki/* /wiki/
 python3 /create_sidebar.py
-cd /wiki
 git add .
 git commit -m "Automatic Wiki Deployment"
 git push -f origin master
