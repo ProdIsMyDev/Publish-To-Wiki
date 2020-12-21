80cd07ce915db79cb5d95a6453b58cccef6a9814
CKO <chris@casualcode.org>
2020-12-21 14:01:30 +0100

    work work work

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ git config --global user.name "$GITHUB_ACTOR"
 git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com"
 
 git clone "https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.git" /repo
+WIKI_URL="https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.wiki.git"
 
 cd /
 
@@ -15,12 +16,12 @@ yes | cp -rf /repo/wiki/* /wiki/
 python3 /create_sidebar.py
 
 cd /wiki
+
 git init
 git config user.name "$GITHUB_ACTOR"
 git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
-echo "$GITHUB_ACTOR@users.noreply.github.com"
-echo "https://$GITHUB_ACTOR:$GH_PERSONAL_ACCESS_TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git"
-git remote set-url origin "https://$GITHUB_ACTOR:$GH_PERSONAL_ACCESS_TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git"
+git pull "$WIKI_URL"
+
 git add .
 git commit -m "Automatic Wiki Deployment"
-git push origin master
+git push --set-upstream "$WIKI_URL" master
