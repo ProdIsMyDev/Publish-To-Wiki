15fe87455a4fa2e296f0387cb050133b764b755e
CKO <chris@casualcode.org>
2020-12-21 04:36:02 +0100

    it is getting late...

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,12 @@ git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com"
 git config --global user.email "you@example.com"
 
 git clone "https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.git" /repo
-git clone "https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.wiki.git" /wiki
 
-rm -rf /wiki/*
-cp -r /repo/wiki/* /wiki/
+mkdir /wiki
+git init
+git pull https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.wiki.git /wiki
+yes | cp -rf /repo/wiki/* /wiki/
 python3 /create_sidebar.py
 git add .
 git commit -m "Automatic Wiki Deployment"
-git push -f origin master
+git push --set-upstream https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.wiki.git master
